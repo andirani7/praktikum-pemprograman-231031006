a062a44af66aab9dc46ef83b7f962eb6deeee3b4
andirani7 <153787965+andirani7@users.noreply.github.com>
2023-12-21 19:54:13 +0800

    Pengenalan Variabel

---
+++ praktikum-a1.py
@@ -0,0 +1,107 @@
+print('Praktikum-1')
+# Print Nama Lengkap, Nim, Prodi
+print ('Nama Lengkap : Andi Maharani')
+print ('Nim          : 231031006')
+print ('Prodi        : Sistem Informasi')
+print () 
+x=2023
+print('Angkatan',x)
+print ()
+a=10
+print('Data adalah: ',a)
+print('tipe data a adalah',type(a))
+print ()
+
+b=10.0
+print('Data adalah: ',b)
+print('tipe data b adalah',type(b))
+print()
+
+kampus = 'Institut Teknologi BJ Habibie'
+print('Data adalah; ',kampus)
+print('tipe data adalah', type(kampus))
+print()
+
+c=complex(2,3)
+print('Data adalah: ',c)
+print('tipe data b adalah',type(c))
+print()
+
+log = True
+print('Data adalah: ',log)
+print('tipe data b adalah',type(log))
+print()
+
+p=20
+q=23
+hasil = p + q
+print('Hasil',p,'+',q,'=',hasil)
+print('Ini adalah batas untuk tugas praktikum######')
+
+print('Penjumlahan')
+a=4
+b=5
+hasil = a + b
+print('Hasil',a,'+',b,'=',hasil)
+print()
+
+print('pengurangan')
+a=6
+b=7
+hasil = a + b
+print('Hasil',a,'-',b,'=',hasil)
+print()
+
+print('Perkalian')
+a=7
+b=4
+hasil = a * b
+print('Hasil',a,'*',b,'=',hasil)
+print()
+
+print('Pembagian')
+a=4
+b=6
+hasil = a / b
+print('Hasil',a,'/',b,'=',hasil)
+print()
+
+print('Modulus')
+a=4
+b=5
+hasil = a % b
+print('Hasil',a,'%',b,'=',hasil)
+print()
+
+print('Eksponensial')
+a=4
+b=5
+hasil = a ** b
+print('Hasil',a,'**',b,'=',hasil)
+print()
+
+print('Eksponensial')
+a=4
+b=5
+hasil = a ** b
+print('Hasil',a,'**',b,'=',hasil)
+print()
+
+print('Eksponensial')
+a=4
+b=5
+hasil = a ** b
+print('Hasil',a,'**',b,'=',hasil)
+print()
+
+print('Floor difision')
+a=4
+b=5
+hasil = a // b
+print('Hasil',a,'//',b,'=',hasil)
+print()
+
+
+
+
+
